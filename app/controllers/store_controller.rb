ccb8e033d12c3116565acff3303c0856d9209de9
ChrisBaragar <chrisb@nulogy.com>
2013-03-15 11:59:25 -0400

    C5 done!

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,5 +1,11 @@
 class StoreController < ApplicationController
   def index
   	@products = Product.order(:title)
+  	
+  	if session[:store_index_counter].nil?
+  		session[:store_index_counter] = 0
+  	end
+
+  	session[:store_index_counter] += 1
   end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -23,6 +23,8 @@
 class Product < ActiveRecord::Base
   has_many :line_items
 
+  before_destroy :ensure_not_referenced_by_any_line_item
+
   validates :title, :description, :image_url, presence: true
   validates :price, numericality: {greater_than_or_equal_to: 0.01}
   validates :title, uniqueness: true, length: {minimum: 10}
@@ -31,7 +33,6 @@ class Product < ActiveRecord::Base
     message: 'must be a URL for GIF, JPG or PNG image.'
   }
 
-  before_destroy :ensure_not_reference_by_any_line_item
 
   # Hey idiot, define this inside of the class, not out in the wild!
   def self.latest
@@ -39,6 +40,8 @@ class Product < ActiveRecord::Base
   end
 
   private
+    
+    # ensure that there are no line items referencing this product
     def ensure_not_referenced_by_any_line_item
       if line_items.empty?
         return true
